a0bd28b5252d1a8b0e7fe9079499aecbc658a3f9
Fabs <15038234+lumix103@users.noreply.github.com>
2024-08-04 16:56:15 -0700

    Chaged the eventStream to ensure double new line on data and headers are right

--- app/routes/chat/route.tsx
+++ app/routes/chat/route.tsx
@@ -3,16 +3,26 @@ import { eventStream } from "remix-utils/sse/server";
 import { emitter } from "~/utils/emitter.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  return eventStream(request.signal, (send) => {
-    const handle = () => {
-      send({ data: String(Date.now()) });
-    };
+  return eventStream(
+    request.signal,
+    (send) => {
+      const handle = () => {
+        send({ data: String(Date.now()) + "\n\n" });
+      };
 
-    emitter.addListener("chat", handle);
+      emitter.addListener("chat", handle);
 
-    return () => {
-      console.log("emitter removed");
-      emitter.removeListener("chat", handle);
-    };
-  });
+      return () => {
+        console.log("emitter removed");
+        emitter.removeListener("chat", handle);
+      };
+    },
+    {
+      headers: {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+      },
+    }
+  );
 }
